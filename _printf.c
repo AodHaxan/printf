646a50ac9eec3a26d3b437b434140069e8c6ff2f
AodHaxan <hsn.aouidy@gmail.com>
2023-04-25 14:27:55 -0700

    Project

--- _printf.c
+++ _printf.c
@@ -1,16 +1,6 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-
-#define BUFF_SIZE 1024
+#include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-int handle_print(const char *format, int *i, va_list list, char buffer[],
-                 int flags, int width, int precision, int size);
 
 /**
  * _printf - Printf function
@@ -19,78 +9,58 @@ int handle_print(const char *format, int *i, va_list list, char buffer[],
  */
 int _printf(const char *format, ...)
 {
-    int i, printed = 0, printed_chars = 0;
-    int flags, width, precision, size, buff_ind = 0;
-    va_list list;
-    char buffer[BUFF_SIZE];
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(list, format);
+	va_start(list, format);
 
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
-            printed_chars++;
-        }
-        else
-        {
-            print_buffer(buffer, &buff_ind);
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, list);
-            precision = get_precision(format, &i, list);
-            size = get_size(format, &i);
-            ++i;
-            printed = handle_print(format, &i, list, buffer,
-                                   flags, width, precision, size);
-            if (printed == -1)
-                return (-1);
-            printed_chars += printed;
-        }
-    }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-    print_buffer(buffer, &buff_ind);
-    va_end(list);
+	print_buffer(buffer, &buff_ind);
 
-    return (printed_chars);
+	va_end(list);
+
+	return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exists
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: Pointer to index at which to add next char, represents the length.
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0)
-        write(1, &buffer[0], *buff_ind);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-    *buff_ind = 0;
-}
-
-/**
- * get_flags - Returns the flags associated with the format specifier
- * @format: Format string
- * @i: Pointer to index of the format specifier
- * Return: Flags (0 for no flags, 1 for left-justified, 2 for zero-padded)
- */
-int get_flags(const char *format, int *i)
-{
-    int flags = 0;
-
-    while (format[*i] == '-' || format[*i] == '0')
-    {
-        if (format[*i] == '-')
-            flags |= 1;  /* left-justified */
-        else if (format[*i] == '0')
-            flags |= 2;  /* zero-padded */
-        (*i)++;
-    }
-
-    return (flags);
+	*buff_ind = 0;
 }
